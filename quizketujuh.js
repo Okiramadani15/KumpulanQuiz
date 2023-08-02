573d5ec13309d2e4972bb6ccd22c4299c478cde6
okiramadani15 <okiramadani53@gmail.com>
2023-08-03 06:08:16 +0700

    quiz7

--- quizketujuh.js
+++ quizketujuh.js
@@ -1,8 +1,15 @@
-function wierdMultyply(sentence){
-
-    
-}
-
-console.log(39)
-console.log(999)
-console.log(3)
+function wierdMultiply(sentence){
+    if (sentence < 10  ){
+        return sentence
+    }
+    let result = 1
+    let pisah = sentence.toString()
+    let pecah = pisah.split("")
+ for (let i = 0; i < pecah.length;  i++){
+  result *= pecah[i]   
+} 
+ return wierdMultiply(result)
+    }
+console.log(wierdMultiply(39))
+console.log(wierdMultiply(999))
+console.log(wierdMultiply(3))
