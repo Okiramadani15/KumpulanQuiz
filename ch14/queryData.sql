ec0b7a93d6c61e103ad26ed931e190f634874f3f
okiramadani15 <okiramadani53@gmail.com>
2023-08-20 15:27:50 +0700

    challenge14

--- ch14/queryData.sql
+++ ch14/queryData.sql
@@ -47,48 +47,34 @@ CREATE TABLE Mata_kuliah(
      FOREIGN KEY(ID_Jurusan) REFERENCES Jurusan (ID_Jurusan)
 );
 
-INSERT INTO Mata_kuliah ( ID_Matakuliah, Nama_Matakuliah, ID_Dosen, ID_Jurusan,Sks) VALUES 
+INSERT INTO Mata_kuliah ( ID_Matakuliah, Nama_Matakuliah, ID_Dosen, ID_Jurusan,Sks,) VALUES 
 ("MK001","Learn Logic javascrpits","D001","J002", "5"),
 ("MK002","Learn sqlite3","D001","J001", "4"),
 ("MK003","Learn postgreSql", "D001","J001","4");
 
 SELECT * FROM Mata_kuliah;
 
-CREATE TABLE Jadwal (
-    ID_Jurusan VARCHAR (10) NOT NULL,
-    ID_Matakuliah VARCHAR(10) PRIMARY KEY NOT NULL,
-    Sks VARCHAR(10) NOT NULL,
-    FOREIGN KEY(ID_Jurusan) REFERENCES Jurusan (ID_Dosen),
-    FOREIGN KEY(ID_Matakuliah ) REFERENCES Mata_kuliah (ID_Dosen),
-    FOREIGN KEY(Sks) REFERENCES Mata_kuliah(ID_Dosen)
-);
-
-INSERT INTO Jadwal (ID_Jurusan,ID_Matakuliah,Sks) VALUES
-("J002","MK001","5"),
-("J001","MK002","4"),
-("J001","MK003","4");
-
-SELECT * FROM Jadwal;
-
 CREATE TABLE KRS(
     Nim VARCHAR(10) PRIMARY KEY NOT NULL,
     ID_Jurusan VARCHAR (10) NOT NULL,
+    ID_Dosen VARCHAR(10) NOT NULL,
     Nama_Matakuliah VARCHAR(100) NOT NULL,
     Sks VARCHAR(10) NOT NULL,
     Nilai VARCHAR(2) NOT NULL,
     FOREIGN KEY(Nim) REFERENCES Mahasiswa (Nim),
     FOREIGN KEY(ID_Jurusan) REFERENCES Jurusan (ID_Jurusan),
+    FOREIGN KEY(ID_Dosen) REFERENCES Dosen(ID_Dosen),
     FOREIGN KEY(Nama_Matakuliah) REFERENCES Mata_kuliah (Mata_kuliah),
     FOREIGN KEY(Sks) REFERENCES Mata_kuliah (Sks)
 );
 
 INSERT INTO KRS ( Nim,ID_Jurusan,Nama_Matakuliah,Sks,Nilai) VALUES
-("M001","J001","Learn sqlite3","4","A"),
-("M002","J001","Learn sqlite3","4","A"),
-("M003","J001","Learn postgreSql","4","A"),
-("M004","J002","Learn Logic javascrpits","5","A"),
-("M005","J002","Learn Logic javascrpits","5","A"),
-("M006","J002","Learn Logic javascrpits","5","A");
+("M001","J001","D001","Learn sqlite3","4","A"),
+("M002","J001","D001","Learn sqlite3","4","A"),
+("M003","J001","D001","Learn postgreSql","4","A"),
+("M004","J002","D001","Learn Logic javascrpits","5","A"),
+("M005","J002","D001","Learn Logic javascrpits","5","A"),
+("M006","J002","D001","Learn Logic javascrpits","5","A");
 
 SELECT * FROM KRS;
 
